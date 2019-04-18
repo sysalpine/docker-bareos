7052db55540b30cdbdf042bfc24adcc3c21a60eb
Paul Trunk <mail@p7k.org>
2019-04-18 15:19:55 +0200

    Add BAREOS_DIR_NAME env variable

--- bareos-fd/entrypoint.sh
+++ bareos-fd/entrypoint.sh
@@ -4,10 +4,11 @@ sed -i 's@BAREOS_FD_NAME@'"$BAREOS_FD_NAME"'@' /etc/bareos/bareos-fd.conf
 sed -i 's@BAREOS_FD_ADDRESS@'"$BAREOS_FD_ADDRESS"'@' /etc/bareos/bareos-fd.conf
 sed -i 's@BAREOS_FD_PORT@'"$BAREOS_FD_PORT"'@' /etc/bareos/bareos-fd.conf
 sed -i 's@BAREOS_FD_PASSWORD@'"$BAREOS_FD_PASSWORD"'@' /etc/bareos/bareos-fd.conf
+sed -i 's@BAREOS_FD_EXTRA_CONFIG@'"$BAREOS_FD_EXTRA_CONFIG"'@' /etc/bareos/bareos-fd.conf
 sed -i 's@BAREOS_DIR_ADDRESS@'"$BAREOS_DIR_ADDRESS"'@' /etc/bareos/bareos-fd.conf
+sed -i 's@BAREOS_DIR_EXTRA_CONFIG@'"$BAREOS_DIR_EXTRA_CONFIG"'@' /etc/bareos/bareos-fd.conf
+sed -i 's@BAREOS_DIR_NAME@'"$BAREOS_DIR_NAME"'@' /etc/bareos/bareos-fd.conf
 sed -i 's@CONNECTION_FROM_CLIENT_TO_DIRECTOR@'"$CONNECTION_FROM_CLIENT_TO_DIRECTOR"'@' /etc/bareos/bareos-fd.conf
 sed -i 's@CONNECTION_FROM_DIRECTOR_TO_CLIENT@'"$CONNECTION_FROM_DIRECTOR_TO_CLIENT"'@' /etc/bareos/bareos-fd.conf
-sed -i 's@BAREOS_FD_EXTRA_CONFIG@'"$BAREOS_FD_EXTRA_CONFIG"'@' /etc/bareos/bareos-fd.conf
-sed -i 's@BAREOS_DIR_EXTRA_CONFIG@'"$BAREOS_DIR_EXTRA_CONFIG"'@' /etc/bareos/bareos-fd.conf
 
 bareos-fd -f -c /etc/bareos/bareos-fd.conf
